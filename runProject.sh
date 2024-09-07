3a07bf6a4e9c4b693ee720aca78b4f225c3b1447
Srivastava <89432812+Srivastavavisek@users.noreply.github.com>
2024-09-08 04:35:07 +0545

    Deleted

--- runProject.sh
+++
@@ -1,7 +0,0 @@
-MAIN_FILE="main.cpp"
-OUTPUT_FILE="game"
-SRC_FILES="src/Game.cpp src/Player.cpp src/Collisions.cpp src/BackGround.cpp src/Asteroids.cpp src/Bullet.cpp"
-
-g++ $MAIN_FILE $SRC_FILES -o $OUTPUT_FILE -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-
-./$OUTPUT_FILE
